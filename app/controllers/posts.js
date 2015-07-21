fe6627a6d949ace4f0b432b34dbd65896a371061
Nevvea7 <annayen2008@gmail.com>
2015-07-21 12:38:46 -0400

    env variables/structure fix

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,7 +1,6 @@
+"use strict";
 function PostsDAO(db) {
-	"use strict";
-
-
+	
     /* If this constructor is called without the "new" operator, "this" points
      * to the global object. Log a warning and call it correctly. */
     if (false === (this instanceof PostsDAO)) {
@@ -9,13 +8,11 @@ function PostsDAO(db) {
         return new PostsDAO(db);
     }
 
-    var posts = db.collection("fakeposts");
+    var posts = db.collection("posts");
 
     this.getPosts = function(num, callback) {
-    	"use strict";
 
     	posts.find().limit(num).toArray(function(err, items) {
-    		"use strict";
     		if (err) return callback(err, null);
     		console.log("Found posts");
     		callback(err, items);
@@ -23,10 +20,8 @@ function PostsDAO(db) {
     }
 
     this.getPostByLink = function(link, callback) {
-        "use strict";
 
         posts.findOne({'link': link}, function(err, post) {
-            "use strict";
 
             if (err) return callback(err, null);
 
